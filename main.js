a7423d2487dcd3c0a3b9bb8b6971f393bbbe8b58
Carla <carlalima@exemplo.com>
2023-04-20 20:14:15 -0300

    toFixed: limitando casas decimais.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const atividades = [];
 const notas = [];
 const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
 const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
-//const notaMinima = parseFloat(prompt("Digite a nota mínima:"));
+const notaMinima = parseFloat(prompt("Digite a nota mínima:"));
 
 let linhas = '';
 
@@ -31,7 +31,7 @@ function adicionaLinha() {
         let linha = '<tr>';
         linha += `<td>${inputNome.value}</td>`;
         linha += `<td>${inputNota.value}</td>`;
-        linha += `<td>${inputNota.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+        linha += `<td>${inputNota.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
         linha += '<tr>';
 
         linhas += linha;
@@ -49,8 +49,8 @@ function atualizaTabela() {
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
 function calculaMediaFinal() {
